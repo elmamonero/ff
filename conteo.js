bac92930d270005e7aa85e1aea8441f620a01e12
elmamonero <debastado0@gmail.com>
2025-07-25 20:26:14 -0400

    Create conteo.js

---
+++ conteo.js
@@ -0,0 +1,38 @@
+import path from "path";
+import fs from "fs";
+
+const conteoPath = path.resolve("./conteo.js");
+
+// Importar datos con import() dinámico
+async function leerConteo() {
+  const datos = await import(conteoPath + "?update=" + Date.now());
+  return datos.default || {};
+}
+
+function guardarConteo(data) {
+  const contenido = "export default " + JSON.stringify(data, null, 2) + ";\n";
+  fs.writeFileSync(conteoPath, contenido);
+}
+
+async function contarMensaje(msg, conn) {
+  if (!msg.key.remoteJid.endsWith("@g.us")) return;
+
+  const chatId = msg.key.remoteJid;
+  const sender = msg.key.participant || msg.key.remoteJid;
+
+  let conteoData;
+  try {
+    conteoData = await leerConteo();
+  } catch {
+    conteoData = {};
+  }
+
+  if (!conteoData[chatId]) conteoData[chatId] = {};
+  if (!conteoData[chatId][sender]) conteoData[chatId][sender] = 0;
+
+  conteoData[chatId][sender] += 1;
+
+  guardarConteo(conteoData);
+}
+
+export { contarMensaje };
