ae025dbaf61e08ad3849c601fe76acf41ef42fb3
elmamonero <debastado0@gmail.com>
2025-07-25 20:39:28 -0400

    Create contador.js

---
+++ plugins/contador.js
@@ -0,0 +1,37 @@
+import fs from 'fs';
+import path from 'path';
+
+const conteoPath = path.resolve('./conteo.js');
+
+async function leerConteo() {
+  const datos = await import(conteoPath + '?update=' + Date.now());
+  return datos.default || {};
+}
+
+function guardarConteo(data) {
+  const contenido = 'export default ' + JSON.stringify(data, null, 2) + ';\n';
+  fs.writeFileSync(conteoPath, contenido);
+}
+
+async function contarMensaje(msg, conn) {
+  if (!msg.key.remoteJid.endsWith('@g.us')) return; // solo grupos
+
+  const chatId = msg.key.remoteJid;
+  const sender = msg.key.participant || msg.key.remoteJid;
+
+  let conteoData;
+  try {
+    conteoData = await leerConteo();
+  } catch {
+    conteoData = {};
+  }
+
+  if (!conteoData[chatId]) conteoData[chatId] = {};
+  if (!conteoData[chatId][sender]) conteoData[chatId][sender] = 0;
+
+  conteoData[chatId][sender] += 1;
+
+  guardarConteo(conteoData);
+}
+
+export { contarMensaje };
