0b78521642e4de74d52af51e08eb7384928f967c
elmamonero <debastado0@gmail.com>
2025-08-02 16:22:00 -0400

    Update _antiprivado.js

--- plugins/_antiprivado.js
+++ plugins/_antiprivado.js
@@ -1,18 +1,24 @@
 export async function before(m, {conn, isAdmin, isBotAdmin, isOwner, isRowner, isMods}) {
-  if (m.isBaileys && m.fromMe) return true;
-  if (m.isGroup) return false;
-  if (!m.message) return true;
-  if (
-    m.text.includes('PIEDRA') || m.text.includes('PAPEL') || m.text.includes('TIJERA') ||
-    m.text.includes('serbot') || m.text.includes('code') || m.text.includes('qr')
-  ) return true;
+if (m.isBaileys && m.fromMe) return true;
+if (m.isGroup) return false;
+if (!m.message) return true;
+if (
+m.text.includes('PIEDRA') || m.text.includes('PAPEL') || m.text.includes('TIJERA') ||
+m.text.includes('serbot') || m.text.includes('code') || m.text.includes('qr')
+) return true;
 
-  const chat = global.db.data.chats[m.chat];
-  const bot = global.db.data.settings[this.user.jid] || {};
+const chat = global.db.data.chats[m.chat];
+const bot = global.db.data.settings[this.user.jid] || {};
 
-  if (bot.antiPrivate && !isOwner && !isRowner && !isMods) {
-    // Aquí SOLO bloquea, no responde ningún mensaje
-    await this.updateBlockStatus(m.chat, 'block');
-  }
-  return false;
+if (bot.antiPrivate && !isOwner && !isRowner && !isMods) {
+await m.reply(
+`> 《★》@${m.sender.split`@`[0]} Está prohibido escribir al privado del bot.\n\n` +
+`> Para probar el bot, únete al grupo oficial aquí: https://chat.whatsapp.com/HvDCvNqXSiW19MFXJmWhoF\n\n` +
+`> Para seguir nuestro canal oficial, haz clic aquí: ${channel}`,
+false,
+{ mentions: [m.sender] }
+);
+await this.updateBlockStatus(m.chat, 'block');
+}
+return false;
 }
